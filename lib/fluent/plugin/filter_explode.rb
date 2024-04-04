3afeaeee6aede1b62f0be0b519b63d4b6cff7995
Nick Littrell <nlittrell@atlassian.com>
2024-04-04 13:29:23 -0400

    update to work with updated mutate_event

--- lib/fluent/plugin/filter_explode.rb
+++ lib/fluent/plugin/filter_explode.rb
@@ -1,13 +1,13 @@
 require 'fluent/filter'
-require 'fluent/plugin_mixin/mutate_event'
+require 'fluent/plugin/mixin/mutate_event'
 
 module Fluent
   class ExplodeFilter < Filter
     Fluent::Plugin.register_filter('explode', self)
 
     def filter(tag, time, record)
-      event = Fluent::PluginMixin::MutateEvent.new(record, expand_nesting:false)
-      event_data = Fluent::PluginMixin::MutateEvent.new({}, expand_nesting:true)
+      event = Fluent::Plugin::Mixin::MutateEvent.new(record, expand_nesting:false)
+      event_data = Fluent::Plugin::Mixin::MutateEvent.new({}, expand_nesting:true)
 
       event.keys.each do |key|
         event_data.set(key, event.get(key))
